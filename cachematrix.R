1fa541376a60ecee8599005cced7d961fe3937e3
freddy418 <freddy418@gmail.com>
2019-05-06 23:49:09 -0700

    adding comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,10 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
+## set/get functions are accessors for the matrix, any time set is called,
+## the cached matrix inverse is reset
+## set/get functions are accessors for the matrix's inverse
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
@@ -18,7 +21,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## function computes the inverse of the special matrix contained in argument x
+## if the inverse has been computed and a cached result is available, the cached
+## result is used, if not the inverse is recomputed using solve() on special x
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
